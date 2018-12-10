1d5d91e5123909f0cb83152d4e7ade4b37093ea2
Boris Khatkov <bkhatkov@gmail.com>
2018-12-10 10:32:10 +0300

    Fix

--- src/main/java/com/bkhatkov/zookeeperbackendservicedemo/ZookeeperBackendServiceDemoController.java
+++ src/main/java/com/bkhatkov/zookeeperbackendservicedemo/ZookeeperBackendServiceDemoController.java
@@ -1,14 +1,19 @@
 package com.bkhatkov.zookeeperbackendservicedemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZookeeperBackendServiceDemoController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/helloworld")
     public String helloWorld() {
-        return "Hello World! Wooooow!";
+        return "Hello World from: " + environment.getProperty("server.port");
     }
 
 }
