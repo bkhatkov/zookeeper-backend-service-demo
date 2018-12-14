bfa51e156470810078072a4dc9726b3716bc005d
Boris Khatkov <bkhatkov@gmail.com>
2018-12-14 17:01:40 +0300

    Fix

--- src/main/java/com/bkhatkov/zookeeperbackendservicedemo/ZookeeperBackendServiceDemoApplication.java
+++ src/main/java/com/bkhatkov/zookeeperbackendservicedemo/ZookeeperBackendServiceDemoApplication.java
@@ -3,15 +3,21 @@ package com.bkhatkov.zookeeperbackendservicedemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ZookeeperBackendServiceDemoApplication {
 
     @Autowired
+    private Registration registration;
+
+    @Autowired
     private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(ZookeeperBackendServiceDemoApplication.class, args);
+
+
     }
 }
--- src/main/java/com/bkhatkov/zookeeperbackendservicedemo/ZookeeperBackendServiceDemoController.java
+++ src/main/java/com/bkhatkov/zookeeperbackendservicedemo/ZookeeperBackendServiceDemoController.java
@@ -1,6 +1,8 @@
 package com.bkhatkov.zookeeperbackendservicedemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ZookeeperBackendServiceDemoController {
 
     @Autowired
+    private ZookeeperRegistration registration;
+
+    @Autowired
     Environment environment;
 
     @GetMapping("/")
     public String index() {
-        return "Hello from Zookeeper Demo Backend Service";
+        return "Hello from Zookeeper Demo Backend Service " + registration.getMetadata();
     }
 
     @GetMapping("/helloworld")
